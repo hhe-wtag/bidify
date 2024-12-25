007888a1f521b5d002938be73d9f96feddcf0d44
Afnan Alauddin Mumu <afnan.mumu@welldev.io>
2024-12-25 14:47:33 +0600

    feat: Refactor and move user registration logic to user repository

--- backend/src/repositories/BaseRepository.js
+++ backend/src/repositories/BaseRepository.js
@@ -11,27 +11,12 @@ class BaseRepository {
     return this.model.findById(id);
   }
 
-  async findByIdWithPassword(id) {
-    return this.model.findById(id).select('+password');
-  }
-
   async findByEmail(email) {
     return this.model.findOne({ email });
   }
 
-  async findByEmailWithPassword(email) {
-    return this.model.findOne({ email }).select('+password');
-  }
-
   async create(data) {
     return this.model.create(data);
-  }
-
-  async updateById(id, updateData) {
-    return this.model.findByIdAndUpdate(id, updateData, {
-      new: true,
-      runValidators: true,
-    });
   }
 
   async deleteById(id) {
