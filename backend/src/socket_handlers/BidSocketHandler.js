7e46d6f125d380b2aa28e393b45252c6fa0deed2
Hasna Hena Mow <hasna.hena@welldev.io>
2025-01-12 16:16:51 +0600

    chore: separate socket based constants

--- backend/src/socket_handlers/BidSocketHandler.js
+++ backend/src/socket_handlers/BidSocketHandler.js
@@ -1,6 +1,7 @@
 import BaseSocketHandler from './BaseSocketHandler.js';
 import BidSocketRepository from '../repositories/BidSocketRepository.js';
 import HTTP_STATUS from '../utils/httpStatus.js';
+import { EVENTS } from '../utils/socketConstants.js';
 
 class BidSocketHandler extends BaseSocketHandler {
   constructor(io) {
@@ -11,9 +12,9 @@ class BidSocketHandler extends BaseSocketHandler {
   handleJoinItemRoom = (socket, itemId) => {
     socket.join(`item-${itemId}`);
 
-    this.emitToRoom(`item-${itemId}`, 'user-joined-item-room', {
-      event: 'user-joined-item-room',
-      data: { user: socket.user },
+    this.emitToRoom(`item-${itemId}`, EVENTS.USER_JOINED_ROOM, {
+      event: EVENTS.USER_JOINED_ROOM,
+      data: { user: socket.user, room: `item-${itemId}` },
       message: `User-${socket.id} connected to item room ${itemId}`,
     });
   };
@@ -21,9 +22,9 @@ class BidSocketHandler extends BaseSocketHandler {
   handleLeaveItemRoom = (socket, itemId) => {
     socket.leave(`item-${itemId}`);
 
-    this.broadcastToRoom(socket, `item-${itemId}`, 'user-left-item-room', {
-      event: 'user-left-item-room',
-      data: { user: socket.user },
+    this.broadcastToRoom(socket, `item-${itemId}`, EVENTS.USER_LEFT_ROOM, {
+      event: EVENTS.USER_LEFT_ROOM,
+      data: { user: socket.user, room: `item-${itemId}` },
       message: `User-${socket.id} left the room ${itemId}`,
     });
   };
@@ -32,18 +33,12 @@ class BidSocketHandler extends BaseSocketHandler {
     const result = await this.bidSocketRepository.placeBid(bidData);
 
     if (result.statusCode === HTTP_STATUS.CREATED) {
-      this.emitToRoom(`item-${bidData.itemId}`, 'new-bid-placed', {
-        event: 'new-bid-placed',
+      this.emitToRoom(`item-${bidData.itemId}`, EVENTS.NEW_BID_PLACED, {
+        event: EVENTS.NEW_BID_PLACED,
         data: { bid: result },
         message: `New bid of $${bidData.incrementBidAmount} placed by userId: ${bidData.bidderId}`,
       });
     }
-
-    this.emitToUser(socket.id, 'place-bid-result', {
-      event: 'place-bid-result',
-      data: { bid: result },
-      message: `Attempt of 'place-bid' by userId: ${bidData.bidderId} against itemId: ${bidData.itemId}`,
-    });
   };
 }
 
