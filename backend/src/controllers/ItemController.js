d36b664612b38c7c8e00b09d1eeeb4a21f4ff24f
Hasna Hena Mow <hasna.hena@welldev.io>
2025-01-19 12:35:39 +0600

    BID-34: Enlisted and Won Items
    feat: BE user enlisted and won item API
    
    feat: FE user enslisted and won items view and API int.

--- backend/src/controllers/ItemController.js
+++ backend/src/controllers/ItemController.js
@@ -67,12 +67,9 @@ class ItemController extends BaseController {
     const userId = req.user.id;
 
     const existingItem = await this.repository.findById(id);
-    
+
     if (!existingItem) {
-      throw new ApiError(
-        HTTP_STATUS.NOT_FOUND,
-        'Item not found'
-      );
+      throw new ApiError(HTTP_STATUS.NOT_FOUND, 'Item not found');
     }
 
     if (updates.endTime && new Date(updates.endTime) <= new Date()) {
@@ -83,19 +80,19 @@ class ItemController extends BaseController {
     }
 
     const baseURL = `${req.protocol}://${req.get('host')}`;
-    
+
     // Handle image uploads if there are new files
     if (req.files && req.files.length > 0) {
       const imageUploadResult = await this.repository.uploadFiles(
         req.files,
         baseURL
       );
-      
+
       if (imageUploadResult.length === req.files.length) {
         // Combine existing images with new ones
         updates.images = [
           // ...(existingItem.images || []),
-          ...imageUploadResult
+          ...imageUploadResult,
         ];
       }
     }
@@ -106,12 +103,16 @@ class ItemController extends BaseController {
       ...updates,
     };
 
-    const updatedItem = await this.repository.updateItem(id, userId, mergedUpdates);
+    const updatedItem = await this.repository.updateItem(
+      id,
+      userId,
+      mergedUpdates
+    );
 
     res
       .status(HTTP_STATUS.OK)
       .json(new ApiResponse(HTTP_STATUS.OK, updatedItem, 'Item updated!'));
-});
+  });
 
   delete = asyncHandler(async (req, res) => {
     const { id } = req.params;
@@ -122,6 +123,26 @@ class ItemController extends BaseController {
     res
       .status(HTTP_STATUS.OK)
       .json(new ApiResponse(HTTP_STATUS.OK, null, 'Item deleted!'));
+  });
+
+  getUserEnlistedItems = asyncHandler(async (req, res) => {
+    const userId = req.user.id;
+    const items = await this.repository.getUserEnlistedItems(userId);
+
+    res
+      .status(HTTP_STATUS.OK)
+      .json(new ApiResponse(HTTP_STATUS.OK, items, 'User listings found!'));
+  });
+
+  getUserWinningItems = asyncHandler(async (req, res) => {
+    const userId = req.user.id;
+    const items = await this.repository.getUserWinningItems(userId);
+
+    console.log(items);
+
+    res
+      .status(HTTP_STATUS.OK)
+      .json(new ApiResponse(HTTP_STATUS.OK, items, 'User winnings found!'));
   });
 }
 
--- backend/src/routes/item.route.js
+++ backend/src/routes/item.route.js
@@ -7,6 +7,16 @@ import { upload } from '../middleware/multer.js';
 const router = express.Router();
 
 router.get('/all', ItemController.getAll);
+router.get(
+  '/user-enlisted-items',
+  isAuthenticated,
+  ItemController.getUserEnlistedItems
+);
+router.get(
+  '/user-winning-items',
+  isAuthenticated,
+  ItemController.getUserWinningItems
+);
 router.get('/:slug', ItemController.getBySlug);
 router.post(
   '/create',
@@ -14,7 +24,12 @@ router.post(
   upload.array('images', 5),
   ItemController.create
 );
-router.patch('/update/:id', isAuthenticated, upload.array('images', 5), ItemController.update);
+router.patch(
+  '/update/:id',
+  isAuthenticated,
+  upload.array('images', 5),
+  ItemController.update
+);
 router.delete('/delete/:id', isAuthenticated, ItemController.delete);
 
 export const ItemRoutes = router;
